ce8b146191f6efd32d042f74e87684aadc16c3a2
DHung308 <nguyenduonghung30082001@gmail.com>
2023-08-04 10:40:23 +0700

    NguyenDuongHung_Cau6

---
+++ Models/SinhVien.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NguyenDuongHung_01.Models
+{
+    public class SinhVien
+    {
+        [Key]
+        public string MaSinhVien{ get; set; }
+        public string  HoTen { get; set; }
+        public string  Malop { get; set; }
+    }
+}
