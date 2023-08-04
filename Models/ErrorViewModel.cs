23b187816a2d073cef4a003e10c901f2a3a9f020
DHung308 <nguyenduonghung30082001@gmail.com>
2023-08-04 10:15:49 +0700

    NguyenDuongHung_Cau2

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NguyenDuongHung_01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
