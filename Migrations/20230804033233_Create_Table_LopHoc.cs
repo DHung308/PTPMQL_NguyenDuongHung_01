5329c7da03056674fc6cc9caa8952428b19c199b
DHung308 <nguyenduonghung30082001@gmail.com>
2023-08-04 10:36:26 +0700

    NguyenDuongHung_Cau5

---
+++ Migrations/20230804033233_Create_Table_LopHoc.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NguyenDuongHung_01.Migrations
+{
+    /// <inheritdoc />
+    public partial class Create_Table_LopHoc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LopHoc",
+                columns: table => new
+                {
+                    Malop = table.Column<string>(type: "TEXT", nullable: false),
+                    TenLop = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LopHoc", x => x.Malop);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LopHoc");
+        }
+    }
+}
